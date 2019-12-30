055acaaafe6a46b10dbf5016291a4e0cd015bf48
Hung. Ta Nguyen (2) <hungtn2@vng.com.vn>
2019-12-30 15:23:05 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ToDoList v-bind:todos="todos"/>
+    <CreateTodo @add-todo="addTodo"/>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList.vue'
+import CreateTodo from './components/CreateTodo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ToDoList,
+    CreateTodo
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push({
+        title: todo.title,
+        project: todo.project,
+        done: false,
+      });
+    },
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="">
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <ToDo v-for="(todo, index) in todos" @delete-todo="deleteTodo" @complete-todo="completeTodo" v-bind:todo="todo" v-bind:key="index"/>
+  </div>
+</template>
+
+<script>
+import ToDo from './ToDo.vue'
+
+export default {
+  props: ['todos'],
+  components: {
+    ToDo
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    },
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
